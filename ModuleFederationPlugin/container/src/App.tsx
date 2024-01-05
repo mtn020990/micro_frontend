fc836e976c59b054ac63c67de61c65b584b0dc7f
nganmai <ngan@zenfolio.com>
2024-01-05 16:45:09 +0700

    update code

--- ModuleFederationPlugin/container/src/App.tsx
+++ ModuleFederationPlugin/container/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import { Counter } from 'counter/Counter';
+import "./index.css";
+const App = () => (
+  <div className="container">
+    <h1>Container App</h1>
+    <Counter /> // Micro frontend app
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
